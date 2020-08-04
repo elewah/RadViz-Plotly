167427d836065fa3404510f3473cea24f50a094c
elewah <abdelrahman.elewah@gmail.com>
2020-08-04 00:56:13 -0400

    add files that are required to be distributed

--- RadViz/RadViz.py
+++ RadViz/RadViz.py
@@ -123,16 +123,18 @@ def RadViz3DH(y,X,BPs):
     plotRadviz3D(S_hat)
   
 # =============================================================================
-#       
+# for testing purpose     
 # =============================================================================
 
 #BreastCancer
-import plotly.io as pio
-pio.renderers.default = "browser"   
-data= pd.read_csv('DataFolder/BreastCancer.csv')
-y=data['index']
-X=data.drop(['index'], axis=1)
-BPs=10000
-RadViz3D(y,X,BPs)
-RadViz2D(y,X,BPs)  
+# =============================================================================
+# import plotly.io as pio
+# pio.renderers.default = "browser"   
+# data= pd.read_csv('DataFolder/BreastCancer.csv')
+# y=data['index']
+# X=data.drop(['index'], axis=1)
+# BPs=10000
+# RadViz3D(y,X,BPs)
+# RadViz2D(y,X,BPs)  
+# =============================================================================
 
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RadViz-Plotly-elewah", # Replace with your own username
+    version="0.0.1",
+    author="elewah",
+    author_email="abdelrahman.elewah@gmail.com",
+    description="2D and 3D RadViz library, using plotly 2D and 3D scatter plot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elewah/RadViz-Plotly.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

0b535f94a0838d119ccc8c87848c34e5988ea3fc
elewah <abdelrahman.elewah@gmail.com>
2023-05-03 23:27:10 -0400

    add test.py file for testing purpose

--- RadViz/main.py
+++ RadViz/main.py
@@ -125,14 +125,12 @@ def RadViz3DH(y,X,BPs):
 # =============================================================================
 
 #BreastCancer
-# =============================================================================
-# import plotly.io as pio
-# pio.renderers.default = "browser"   
-# data= pd.read_csv('DataFolder/BreastCancer.csv')
-# y=data['index']
-# X=data.drop(['index'], axis=1)
-# BPs=10000
-# RadViz3D(y,X,BPs)
-# RadViz2D(y,X,BPs)  
-# =============================================================================
+import plotly.io as pio
+pio.renderers.default = "browser"   
+data= pd.read_csv('DataFolder/BreastCancer.csv')
+y=data['index']
+X=data.drop(['index'], axis=1)
+BPs=10000
+RadViz3D(y,X,BPs)
+RadViz2D(y,X,BPs)  
 
